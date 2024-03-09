6e848033faffd53accd023a2c5d889a9eca87a52
tyagidevansh <devanshtyagi0607@gmail.com>
2024-03-09 20:31:48 +0530

    works!!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,52 @@
+import { useEffect, useState } from "react";
 import ToDo from "./ToDo";
+import { addToDo, getAllToDo, updateToDo, deleteToDo } from "./utils/HandleAPI";
 
 function App() {
+
+  const [toDo, setToDo] = useState([]);
+  const [text, setText] = useState("");
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [toDoId, setToDoId] = useState("")
+
+  useEffect(() => {
+    getAllToDo(setToDo)
+  }, [])
+
+  const updateMode = (_id, text) => {
+    setIsUpdating(true);
+    setText(text);
+    setToDoId(_id);
+  }
+
   return (
     <div className="App">
       <div className = "container">
         <h1> ToDo App</h1>
         <div className = "top">
-          <input type = "text" placeholder = "Add ToDo's"/>
-          <div className="add"> Add </div>
+          <input 
+            type = "text" 
+            placeholder = "Add ToDo's"
+            value = {text}
+            onChange={(e) => setText(e.target.value)}
+            />
+
+          <div 
+            className="add" 
+            onClick={isUpdating ? 
+            () => updateToDo(toDoId, text, setToDo, setText, setIsUpdating)
+            : ()=>addToDo(text, setText, setToDo)}>
+            {isUpdating? "Update" : "Add"} 
+          </div>
         </div>
         <div className="list">
 
-          <ToDo text = "hi" />
-          <ToDo text = "hi" />
-          <ToDo text = "hi" />
-          
+          {toDo.map((item) => <ToDo 
+          key = {item._id} 
+          text = {item.text} 
+          updateMode={() => {updateMode(item._id, item.text)}}
+          deleteToDo = {() => deleteToDo(item._id, setToDo)}
+          />)}
         </div>
       </div>
     </div>
---
+++ frontend/src/utils/HandleAPI.js
@@ -0,0 +1,54 @@
+import axios from 'axios'
+
+const baseURL = "http://localhost:5000"
+
+const getAllToDo = (setToDo) => {
+    axios
+        .get(baseURL)
+        .then(({data}) => {
+            console.log('data--> ', data);
+            setToDo(data);
+        })
+        .catch((error) => {
+            console.error('Error fetching todos:', error);
+        });
+};
+
+const addToDo = (text, setText, setToDo) => {
+    axios  
+        .post(`${baseURL}/save`, {text})
+        .then((data) => {
+            console.log(data);
+            setText("");
+            getAllToDo(setToDo);
+        })
+        .catch((error) => {
+            console.error('Error adding todo:', error);
+        });
+}
+
+const updateToDo = (toDoId, text, setToDo, setText, setIsUpdating) => {
+    axios  
+        .post(`${baseURL}/update`, {_id: toDoId, text})
+        .then((data) => {
+            setText("");
+            setIsUpdating(false);
+            getAllToDo(setToDo);
+        })
+        .catch((error) => {
+            console.error('Error updating todo:', error);
+        });
+}
+
+const deleteToDo = (_id, setToDo) => {
+    axios  
+        .post(`${baseURL}/delete`, {_id: _id})
+        .then((data) => {
+            getAllToDo(setToDo);
+        })
+        .catch((error) => {
+            console.error('Error updating todo:', error);
+        });
+}
+
+export{getAllToDo, addToDo, updateToDo, deleteToDo};
